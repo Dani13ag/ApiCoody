5841cb1673ec0a1f04f85cd4d5de3e4efabd3b51
luisita53845 <luisita53845@gmail.com>
2020-12-10 17:13:56 -0500

    Settings for the postulation and user modules

--- controllers/postulation.js
+++ controllers/postulation.js
@@ -1,65 +1,85 @@
-const PostulationModel =require('../models/postulation') //estamos requiriendo el modelo
+const PostulationModel = require('../models/postulation')
+
+//Create
 
 exports.create = (req, res) => {
-    //validamos que todos los datos del formulario esten llenos
-    if (Object.entries(req.body).length == 0) {
-       return res.status(400).send({
-          message: 'los datos son obligatorios'
-
-      })
-    }
-//vamos a crear una nueva postulacion
-    const postulation = new PostulationModel({
-        titlePostulation: req.body.titlePostulation,
-        description:  req.body.description,
-        cargo:  req.body. cargo,
-        salary: req.body.salary,
-        languaje:req.body.languaje
-    })
-    
-
-
-   
-    postulation.save()// save metodo de mongoose
-        .then((dataPostulation) => { res.send(dataPostulation) })//si hace esto bien
-        .catch((error) => {//si no ejecute el cath
-            res.status(500) - send({//estado 500 (error de servidor)
-                message: error.message//este mensaje devolvera el error que mongoose tiene 
-            })
-        })
-}
-
-/**
- * Metodo para actualizar una empresa
- */
-
- exports.update=(req,res)=>{
-     //validacion de que todos los campos que se van a actualizar esten llenos 
     if (Object.entries(req.body).length == 0) {
         return res.status(400).send({
             message: 'los datos son obligatorios'
+        });
+    }
+    const postulation = new PostulationModel({
+        titlePostulation: req.body.titlePostulation,
+        description: req.body.description,
+        cargo: req.body.cargo,
+        salary: req.body.salary,
+        languaje: req.body.languaje,
+        status: req.body.status
+    });
 
+    postulation.save()
+        .then((dataPostulation) => { res.send(dataPostulation) })
+        .catch((error) => {
+            res.status(500) - send({
+                message: error.message
+            })
+        });
+}
+
+//Update 
+
+exports.update = (req, res) => {
+    if (Object.entries(req.body).length == 0) {
+        return res.status(400).send({
+            message: 'los datos son obligatorios'
         })
     }
 
-    const postulation= {
+    const postulation = {
         titlePostulation: req.body.titlePostulation,
-        description:  req.body.description,
-        cargo:  req.body. cargo,
+        description: req.body.description,
+        cargo: req.body.cargo,
         salary: req.body.salary,
-        languaje:req.body.languaje
- }
- //findByIdAndUpdate= metodo de mongoose que permite buscar por id y actualizar
- PostulationModel.findByIdAndUpdate(req.params.id, postulation)
+        languaje: req.body.languaje,
+        status: req.body.status
+    }
+
+    PostulationModel.findByIdAndUpdate(req.params.id, postulation)
         .then(
             (postulationUpdate) => {
-                res.send(postulationUpdate)
+                res.send(postulationUpdate);
             }
         ).catch(
             (error) => {
                 res.status(500).send({
                     message: error.message
-                })
+                });
             }
-        )
+        );
+}
+
+//Get All
+
+exports.getAll = (req, res) => {
+    PostulationModel.find()
+        .then((postulation) => {
+            res.send(postulation);
+        })
+        .catch((error) => {
+            res.status(500).send({
+                message: error.message
+            });
+        });
+}
+
+//Get One
+
+exports.getOne = (req, res) => {
+    PostulationModel.findById(req.params.id)
+        .then((postulation) => {
+            res.send(postulation);
+        })
+        .catch((error) => {
+            res.status(500).send({ message: error.message });
+        });
 }
--- models/postulation.js
+++ models/postulation.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const postulationSchema = new mongoose.Schema({
     titlePostulation: { type: String, required: true },
-    description:  { type: String, required: true },
-    cargo: { type: String, required: true},
+    description: { type: String, required: true },
+    cargo: { type: String, required: true },
     salary: { type: Number, required: true },
-    languaje:{ type: String, required: true }
-})
+    languaje: { type: String, required: true },
+    status: { type: String, required: true }
+});
 
 
-module.exports = mongoose.model('Postulation', postulationSchema)
+module.exports = mongoose.model('Postulation', postulationSchema);
